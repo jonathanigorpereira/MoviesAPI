a3bb54c47b31b732625efb4f348bd34ce32babfb
Jonathan Bockorny <jonathan.igor.pereira@hotmail.com>
2023-02-12 18:47:07 -0300

    novas features

---
+++ MoviesAPI/MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System;
+using System.IO;
+using System.Linq;
+
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController: ControllerBase
+    {
+        private static List<Filme> movies = new List<Filme>();
+        private static int id = 0;
+
+        [HttpPost]
+        public IActionResult InsertMovie([FromBody] Filme movie)
+        {
+            movie.ID = id++;
+            movies.Add(movie);
+            return CreatedAtAction(nameof(GetMovieByID), new { id = movie.ID}, movie);
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> Get([FromQuery] int skip = 0, [FromQuery] int take = 50)
+        {
+            return movies.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovieByID(int id)
+        {
+            var movieByid = movies.FirstOrDefault(x => x.ID == id);
+            if (movieByid == null) return NotFound();
+            return Ok(movieByid);
+        }
+
+
+
+
+    }
+}
---
+++ MoviesAPI/MoviesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "O Título do filme não foi informado.")]   
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "O Gênero do filme não foi informado.")]
+        [MaxLength(50, ErrorMessage ="O tamanho do Gênero não pode ultrapassar 50 caracteres.")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Range(70, 600, ErrorMessage ="A duração do filme deve ter entre 70 e 600 minutos.")]
+        public int Duration { get; set; }
+
+    }
+}
